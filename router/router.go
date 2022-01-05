e8baeacf0669cbd4448bfedda247348b526d38c1
Sourav <ss12011999@gmail.com>
2022-01-05 22:49:26 +0530

    Add home url

--- router/router.go
+++ router/router.go
@@ -2,17 +2,22 @@ package router
 
 import (
 	"assignment1/controller"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func Router() *mux.Router{
-	r:=mux.NewRouter();
-	r.HandleFunc("/createUser",controller.InsertUserController).Methods("POST")
-	r.HandleFunc("/getUsers",controller.GetAllUsersController).Methods("GET")
-	r.HandleFunc("/deleteUser/{id}",controller.DeleteUserController).Methods("DELETE")
-	r.HandleFunc("/updateUser/{id}",controller.UpdateUserController).Methods("PUT")
-	r.HandleFunc("/getUser/{id}",controller.GetUserController).Methods("GET")
-	r.HandleFunc("/deleteAllUsers",controller.DeleteAllUsersController).Methods("DELETE")
-	return r;
+func Router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", seveHome)
+	r.HandleFunc("/createUser", controller.InsertUserController).Methods("POST")
+	r.HandleFunc("/getUsers", controller.GetAllUsersController).Methods("GET")
+	r.HandleFunc("/deleteUser/{id}", controller.DeleteUserController).Methods("DELETE")
+	r.HandleFunc("/updateUser/{id}", controller.UpdateUserController).Methods("PUT")
+	r.HandleFunc("/getUser/{id}", controller.GetUserController).Methods("GET")
+	r.HandleFunc("/deleteAllUsers", controller.DeleteAllUsersController).Methods("DELETE")
+	return r
+}
+func seveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Golang Task.Please go though api documention to use api(https://documenter.getpostman.com/view/19028114/UVXbuzHL)</h1>"))
 }
